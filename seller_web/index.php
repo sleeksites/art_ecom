bb6b7c7972471a46d4b6623f5a9297d9c101bd61
jishant07 <jishanta@gmail.com>
2019-09-20 09:36:34 +0530

    major commit

---
+++ seller_web/index.php
@@ -0,0 +1,118 @@
+<?php include 'header.php' ?>
+    
+    <div class="hero-wrap img" style="background-image: url(https://images.unsplash.com/photo-1564934304050-e9bb87a29c13?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80);">
+      <div class="overlay"></div>
+      <div class="container">
+      	<div class="row d-md-flex no-gutters slider-text align-items-center justify-content-center">
+	        <div class="col-md-10 d-flex align-items-center ftco-animate">
+	        	<div class="text pt-5 mt-md-5" style="text-align:center;margin:auto;">
+	            <h1 class="mb-5 text-center" >"Find the Art, In your Kart"</h1>
+	            	<h3 class="mb-4">Find the Art you need for your Home</h3>
+	          </div>
+	        </div>
+	    	</div>
+      </div>
+    </div>
+    <section class="ftco-section services-section">
+      <div class="container">
+        <div class="row d-flex">
+          <div class="col-md-3 d-flex align-self-stretch ftco-animate">
+            <div class="media block-6 services d-block">
+              <div class="icon"><span class="flaticon-resume"></span></div>
+              <div class="media-body">
+                <h3 class="heading mb-3">Search Millions of Jobs</h3>
+                <p>A small river named Duden flows by their place and supplies.</p>
+              </div>
+            </div>      
+          </div>
+          <div class="col-md-3 d-flex align-self-stretch ftco-animate">
+            <div class="media block-6 services d-block">
+              <div class="icon"><span class="flaticon-team"></span></div>
+              <div class="media-body">
+                <h3 class="heading mb-3">Easy To Manage Jobs</h3>
+                <p>A small river named Duden flows by their place and supplies.</p>
+              </div>
+            </div>    
+          </div>
+          <div class="col-md-3 d-flex align-self-stretch ftco-animate">
+            <div class="media block-6 services d-block">
+              <div class="icon"><span class="flaticon-career"></span></div>
+              <div class="media-body">
+                <h3 class="heading mb-3">Top Careers</h3>
+                <p>A small river named Duden flows by their place and supplies.</p>
+              </div>
+            </div>      
+          </div>
+          <div class="col-md-3 d-flex align-self-stretch ftco-animate">
+            <div class="media block-6 services d-block">
+              <div class="icon"><span class="flaticon-employees"></span></div>
+              <div class="media-body">
+                <h3 class="heading mb-3">Search Expert Candidates</h3>
+                <p>A small river named Duden flows by their place and supplies.</p>
+              </div>
+            </div>      
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="ftco-section ftco-candidates bg-primary">
+    	<div class="container">
+    		<div class="row justify-content-center pb-3">
+          <div class="col-md-10 heading-section heading-section-white text-center ftco-animate">
+          	<span class="subheading">Candidates</span>
+            <h2 class="mb-4">Latest Candidates</h2>
+          </div>
+        </div>
+    	</div>
+    	<div class="container">
+        <div class="row">
+        	<div class="col-md-12 ftco-animate">
+        		<div class="carousel-candidates owl-carousel">
+        			<div class="item">
+		        		<a href="#" class="team text-center">
+		        			<div class="img" style="background-image: url(sell_images/1img.jpeg);"></div>
+		        			<h2>Danica Lewis</h2>
+		        			<span class="position">Western City, UK</span>
+		        		</a>
+        			</div>
+        			<div class="item">
+	        			<a href="#" class="team text-center">
+		        			<div class="img" style="background-image: url(sell_images/2img.jpeg);"></div>
+		        			<h2>Nicole Simon</h2>
+		        			<span class="position">Western City, UK</span>
+		        		</a>
+	        		</div>
+	        		<div class="item">
+	        			<a href="#" class="team text-center">
+		        			<div class="img" style="background-image: url(sell_images/3img.jpeg);"></div>
+		        			<h2>Cloe Meyer</h2>
+		        			<span class="position">Western City, UK</span>
+		        		</a>
+	        		</div>
+	        		<div class="item">
+	        			<a href="#" class="team text-center">
+		        			<div class="img" style="background-image: url(sell_images/4img.jpeg);"></div>
+		        			<h2>Rachel Clinton</h2>
+		        			<span class="position">Western City, UK</span>
+		        		</a>
+	        		</div>
+	        		<div class="item">
+	        			<a href="#" class="team text-center">
+		        			<div class="img" style="background-image: url(sell_images/5img.jpeg);"></div>
+		        			<h2>Dave Buff</h2>
+		        			<span class="position">Western City, UK</span>
+		        		</a>
+	        		</div>
+	        		<div class="item">
+	        			<a href="#" class="team text-center">
+		        			<div class="img" style="background-image: url(sell_images/6img.jpeg);"></div>
+		        			<h2>Dave Buff</h2>
+		        			<span class="position">Western City, UK</span>
+		        		</a>
+	        		</div>
+        		</div>
+        	</div>
+        </div>
+    	</div>
+    </section>		
+<?php include "footer.php" ?>
---
+++ seller_web/signup/index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sign Up</title>
+
+    <!-- Font Icon -->
+    <link rel="stylesheet" href="fonts/material-icon/css/material-design-iconic-font.min.css">
+
+    <!-- Main css -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body style="background:navy;">
+
+    <div class="main">
+        <div class="container">
+            <div class="signup-content">
+                <div class="signup-img">
+                    <img src="https://images.unsplash.com/photo-1564749210719-3aafef9af5fe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1536&q=80" alt="">
+                </div>
+                <div class="signup-form">
+                    <form method="POST" class="register-form" id="register-form">
+                        <h2>student registration form</h2>
+                        <div class="form-row">
+                            <div class="form-group">
+                                <label for="name">First Name :</label>
+                                <input type="text" name="fname" id="name" required/>
+                            </div>
+                            <div class="form-group">
+                                <label for="father_name">Last Name :</label>
+                                <input type="text" name="lname" id="father_name" required/>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="address">Address :</label>
+                            <input type="text" name="address" id="address" required/>
+                        </div>
+                        <div class="form-radio">
+                            <label for="gender" class="radio-label">Gender :</label>
+                            <div class="form-radio-item">
+                                <input type="radio" name="gender" id="male" checked>
+                                <label for="male">Male</label>
+                                <span class="check"></span>
+                            </div>
+                            <div class="form-radio-item">
+                                <input type="radio" name="gender" id="female">
+                                <label for="female">Female</label>
+                                <span class="check"></span>
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="form-group">
+                                <label for="state">State :</label>
+                                <div class="form-select">
+                                    <input type="text" name="state">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="city">City :</label>
+                                <div class="form-select">
+                                    <input type="text" name="city">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="birth_date">DOB :</label>
+                            <input type="date" name="birth_date" id="birth_date">
+                        </div>
+                        <div class="form-group">
+                            <label for="pincode">Pincode :</label>
+                            <input type="number" name="pincode" id="pincode">
+                        </div>
+                        <div class="form-group">
+                            <label for="email">Email ID :</label>
+                            <input type="email" name="email" id="email" />
+                        </div>
+                        <div class="form-submit">
+                            <input type="submit" value="Submit Form" class="submit" name="submit" id="submit" />
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <!-- JS -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="js/main.js"></script>
+</body>
+</html>
